9533e411c84d3dab2b221e0a168ce729309e2a76
Enrique Ansa <enriqueansa@gmail.com>
2023-10-20 13:00:31 -0300

    adding RouterModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPrincipalComponent } from './modules/pokemons/pages/layout-principal/layout-principal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPrincipalComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
